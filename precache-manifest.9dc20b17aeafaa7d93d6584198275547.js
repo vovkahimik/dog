70f7742837506ec2a47abacff552d8af675053f7
vovkahimik <vovkahimik@gmail.com>
2020-05-28 16:49:44 +0300

    Updates

---
+++ precache-manifest.9dc20b17aeafaa7d93d6584198275547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130dfd9ea5fae83d5b91433349edfaed",
+    "url": "/dog/index.html"
+  },
+  {
+    "revision": "7e8766d1f71b32a8e783",
+    "url": "/dog/static/css/main.4df713a5.chunk.css"
+  },
+  {
+    "revision": "c385e3f79d99b3be3ae8",
+    "url": "/dog/static/js/2.a894618e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog/static/js/2.a894618e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8766d1f71b32a8e783",
+    "url": "/dog/static/js/main.1d403677.chunk.js"
+  },
+  {
+    "revision": "6cf379fd5cafb9785cc2",
+    "url": "/dog/static/js/runtime-main.c37502f7.js"
+  },
+  {
+    "revision": "f5ede55382d756440018ff17900d5bcb",
+    "url": "/dog/static/media/animal_bird_duck_quack_003.f5ede553.mp3"
+  }
+]);
